7b52092d82f5ad1a4e056dcf043df1b57545189e
Imany Imany <imany.apk@gmail.com>
2022-11-28 13:46:30 +0000

    Finish DRF views

--- blango/urls.py
+++ blango/urls.py
@@ -44,7 +44,7 @@ urlpatterns = [
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("ip/", blog.views.get_ip),
 
-    path("api/v1/", include("blog.api_urls")),
+    path("api/v1/", include("blog.api.urls")),
 ]
 
 if settings.DEBUG:
