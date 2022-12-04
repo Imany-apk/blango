3ee41f8dca5aeec5a89778411ccfbfdb76ef9d57
Imany Imany <imany.apk@gmail.com>
2022-12-04 05:32:17 +0000

    Finish filtering

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -55,4 +55,9 @@ router.register("tags", TagViewSet)
 router.register("posts", PostViewSet)
 urlpatterns += [
     path("", include(router.urls)),
+    path(
+        "posts/by-time/<str:period_name>/",
+        PostViewSet.as_view({"get": "list"}),
+        name="posts-by-time",
+    ),
 ]
